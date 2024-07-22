e8378f94372656d2fdf7ef51e87271bddc937807
tkuramot <mabdman512@gmail.com>
2024-07-22 21:57:03 +0900

    update group iterator

--- ex02/src/GroupIterator.hpp
+++ ex02/src/GroupIterator.hpp
@@ -1,3 +1,7 @@
+#ifndef GROUP_ITERATOR_HPP
+#define GROUP_ITERATOR_HPP
+
+#include <algorithm>
 #include <iterator>
 
 template <typename Iterator> class GroupIterator {
@@ -26,31 +30,109 @@ public:
     std::advance(it_, size_);
     return *this;
   }
+
   GroupIterator operator--() {
     GroupIterator tmp = *this;
     std::advance(it_, -size_);
     return tmp;
   }
 
-  // Comparison operators
-  bool operator==(const GroupIterator &other) const {
-    return BastIt() == other.BastIt();
-  }
-  bool operator!=(const GroupIterator &other) const {
-    return BastIt() != other.BastIt();
-  }
-
-  // Arithmetic operators
   GroupIterator &operator+=(difference_type n) {
     std::advance(it_, n * size_);
     return *this;
   }
+
   GroupIterator &operator-=(difference_type n) {
     std::advance(it_, -n * size_);
     return *this;
+  }
+
+  // Comparison operators
+  bool operator==(const GroupIterator &other) const {
+    return BastIt() == other.BastIt();
+  }
+
+  bool operator!=(const GroupIterator &other) const {
+    return BastIt() != other.BastIt();
   }
 
 private:
   Iterator it_;
   difference_type size_;
 };
+
+// Relational operators
+template <typename Iterator>
+bool operator<(const GroupIterator<Iterator> &it1,
+               const GroupIterator<Iterator> &it2) {
+  return it1.BastIt() < it2.BastIt();
+}
+
+template <typename Iterator>
+bool operator>(const GroupIterator<Iterator> &it1,
+               const GroupIterator<Iterator> &it2) {
+  return it1.BastIt() > it2.BastIt();
+}
+
+template <typename Iterator>
+bool operator<=(const GroupIterator<Iterator> &it1,
+                const GroupIterator<Iterator> &it2) {
+  return it1.BastIt() <= it2.BastIt();
+}
+
+template <typename Iterator>
+bool operator>=(const GroupIterator<Iterator> &it1,
+                const GroupIterator<Iterator> &it2) {
+  return it1.BastIt() >= it2.BastIt();
+}
+
+// Arithmetic operators
+template <typename Iterator>
+GroupIterator<Iterator>
+operator+(GroupIterator<Iterator> it,
+          typename GroupIterator<Iterator>::difference_type n) {
+  return GroupIterator<Iterator>(it.BastIt() + n * it.Size(), it.Size());
+}
+
+template <typename Iterator>
+GroupIterator<Iterator>
+operator+(typename GroupIterator<Iterator>::difference_type n,
+          GroupIterator<Iterator> it) {
+  return GroupIterator<Iterator>(it.BastIt() + n * it.Size(), it.Size());
+}
+
+template <typename Iterator>
+GroupIterator<Iterator>
+operator-(GroupIterator<Iterator> it,
+          typename GroupIterator<Iterator>::difference_type n) {
+  return GroupIterator<Iterator>(it.BastIt() - n * it.Size(), it.Size());
+}
+
+template <typename Iterator>
+typename GroupIterator<Iterator>::difference_type
+operator-(GroupIterator<Iterator> it1, GroupIterator<Iterator> it2) {
+  return (it1.BastIt() - it2.BastIt()) / it1.Size();
+}
+
+// Swap
+template <typename Iterator>
+void Swap(GroupIterator<Iterator> &it1, GroupIterator<Iterator> &it2) {
+  std::iter_swap(it1.BastIt(), it2.BastIt());
+}
+
+// Construction functions
+template <typename Iterator>
+GroupIterator<Iterator>
+MakeGroupIterator(Iterator it,
+                  typename GroupIterator<Iterator>::difference_type size) {
+  return GroupIterator<Iterator>(it, size);
+}
+
+template <typename Iterator>
+GroupIterator<Iterator>
+MakeGroupIterator(GroupIterator<Iterator> it,
+                  typename GroupIterator<Iterator>::difference_type size) {
+  return GroupIterator<Iterator>(it.BastIt(), size);
+}
+
+#endif // GROUP_ITERATOR_HPP
