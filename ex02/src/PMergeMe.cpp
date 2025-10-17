f2f35d8f53838993eaa617f6d954142803689cfd
tkuramot <mabdman512@gmail.com>
2025-10-17 23:17:29 +0900

    replace list with vector in sortImple vector implementation

--- ex02/src/PMergeMe.cpp
+++ ex02/src/PMergeMe.cpp
@@ -38,25 +38,27 @@ void PMergeMe::sortImpl(
       group_iter(first.base(), 2 * first.size()),
       group_iter(end.base(), 2 * end.size()));
 
-  std::list<group_iter> chain;
+  std::vector<group_iter> chain;
+  chain.reserve(size);
   chain.push_back(first);
   chain.push_back(first + 1);
 
-  std::vector<std::list<group_iter>::iterator> pend;
+  std::vector<diff_t> pend;
+  pend.reserve((size + 1) / 2 - 1 + (has_stray ? 1 : 0));
 
   for (group_iter it = first + 2; it != end; it += 2)
   {
-    std::list<group_iter>::iterator inserted = chain.insert(chain.end(), it + 1);
-    pend.push_back(inserted);
+    chain.push_back(it + 1);
+    pend.push_back(chain.size() - 1);
   }
 
   if (has_stray)
   {
-    pend.push_back(chain.end());
+    pend.push_back(chain.size());
   }
 
   group_iter current_it = first;
-  std::vector<std::list<group_iter>::iterator>::iterator current_pend = pend.begin();
+  std::vector<diff_t>::iterator current_pend = pend.begin();
 
   for (diff_t pow2 = 1, jn = 0, dist = 2;
        dist <= static_cast<diff_t>(pend.end() - current_pend);
@@ -64,15 +66,21 @@ void PMergeMe::sortImpl(
   {
 
     group_iter it = current_it + dist * 2;
-    std::vector<std::list<group_iter>::iterator>::iterator pe = current_pend + dist;
+    std::vector<diff_t>::iterator pe = current_pend + dist;
 
     while (true)
     {
       --pe;
 
-      std::list<group_iter>::iterator insertion_point =
-          binarySearchInsertionPoint(chain.begin(), *pe, it);
-      chain.insert(insertion_point, it);
+      diff_t insertion_index =
+          binarySearchInsertionPoint(chain.begin(), chain.begin() + *pe, it) - chain.begin();
+      chain.insert(chain.begin() + insertion_index, it);
+
+      for (std::vector<diff_t>::iterator idx = current_pend; idx != pend.end(); ++idx)
+      {
+        if (*idx >= insertion_index)
+          ++(*idx);
+      }
 
       if (pe == current_pend)
         break;
@@ -86,16 +94,22 @@ void PMergeMe::sortImpl(
   while (current_pend != pend.end())
   {
     current_it += 2;
-    std::list<group_iter>::iterator insertion_point =
-        binarySearchInsertionPoint(chain.begin(), *current_pend, current_it);
-    chain.insert(insertion_point, current_it);
+    diff_t insertion_index =
+        binarySearchInsertionPoint(chain.begin(), chain.begin() + *current_pend, current_it) - chain.begin();
+    chain.insert(chain.begin() + insertion_index, current_it);
+
+    for (std::vector<diff_t>::iterator idx = current_pend + 1; idx != pend.end(); ++idx)
+    {
+      if (*idx >= insertion_index)
+        ++(*idx);
+    }
     ++current_pend;
   }
 
   std::vector<int> cache;
   cache.reserve(size * first.size());
 
-  for (std::list<group_iter>::iterator it = chain.begin(); it != chain.end(); ++it)
+  for (std::vector<group_iter>::iterator it = chain.begin(); it != chain.end(); ++it)
   {
     std::vector<int>::iterator begin = it->base();
     std::vector<int>::iterator end = it->base();
