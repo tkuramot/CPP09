4bf42819bb791b80b737c5318a3617c2e1781705
tkuramot <mabdman512@gmail.com>
2025-10-26 23:55:53 +0900

    merge insertion sort

--- ex02/src/PMergeMe.hpp
+++ ex02/src/PMergeMe.hpp
@@ -1,204 +1,329 @@
-#ifndef PMERGEME_HPP
-#define PMERGEME_HPP
+#ifndef PMERGE_ME_HPP
+#define PMERGE_ME_HPP
 
 #include <vector>
 #include <deque>
-#include <algorithm>
+#include <iterator>
+#include <iostream>
 
-class PMergeMe
-{
-public:
-  static void sort(std::vector<int>::iterator first,
-                   std::vector<int>::iterator last);
+#ifdef DEBUG_MODE
+#define DEBUG(msg) \
+  do { \
+    std::cerr << "[DEBUG] " << msg << std::endl; \
+  } while(0)
+#else
+#define DEBUG(msg)
+#endif
 
-  static void sort(std::deque<int>::iterator first,
-                   std::deque<int>::iterator last);
+static size_t comparison_count = 0;
 
-private:
-  template <typename Iterator>
-  class GroupIterator
-  {
+class PMergeMe {
   public:
-    typedef Iterator iterator_type;
-    typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
-    typedef typename std::iterator_traits<Iterator>::pointer pointer;
-    typedef typename std::iterator_traits<Iterator>::reference reference;
-
-    GroupIterator() : it_(), size_(0) {}
-    GroupIterator(Iterator it, difference_type size) : it_(it), size_(size) {}
-    GroupIterator(const GroupIterator &other)
-        : it_(other.it_), size_(other.size_) {}
-    GroupIterator &operator=(const GroupIterator &other)
-    {
-      if (this == &other)
-      {
-        return *this;
-      }
-      it_ = other.it_;
-      size_ = other.size_;
-      return *this;
+    template <typename T>
+    static void Sort(std::vector<T> &vec) {
+#ifdef DEBUG_MODE
+      comparison_count = 0;
+#endif
+      GroupIterator<typename std::vector<T>::iterator> first(vec.begin(), 1);
+      GroupIterator<typename std::vector<T>::iterator> last(vec.end(), 1);
+      SortImplVec<std::vector<T> >(first, last);
+#ifdef DEBUG_MODE
+      std::cout << "comparison count: " << comparison_count << std::endl;
+#endif
     }
-    ~GroupIterator() {}
-
-    iterator_type base() const { return it_; }
-    difference_type size() const { return size_; }
-
-    reference operator*() const
-    {
-      Iterator tmp = it_;
-      std::advance(tmp, size_ - 1);
-      return *tmp;
-    }
-
-    pointer operator->() const
-    {
-      return &(operator*());
-    }
-
-    GroupIterator &operator++()
-    {
-      std::advance(it_, size_);
-      return *this;
-    }
-
-    GroupIterator &operator--()
-    {
-      std::advance(it_, -size_);
-      return *this;
-    }
-
-    GroupIterator &operator+=(difference_type n)
-    {
-      it_ += size_ * n;
-      return *this;
-    }
-
-    GroupIterator &operator-=(difference_type n)
-    {
-      it_ -= size_ * n;
-      return *this;
-    }
-
-    bool operator==(const GroupIterator &rhs) const
-    {
-      return it_ == rhs.it_;
-    }
-
-    bool operator!=(const GroupIterator &rhs) const
-    {
-      return it_ != rhs.it_;
-    }
-
-    bool operator<(const GroupIterator &rhs) const
-    {
-      return it_ < rhs.it_;
-    }
-
-    bool operator<=(const GroupIterator &rhs) const
-    {
-      return it_ <= rhs.it_;
-    }
-
-    bool operator>(const GroupIterator &rhs) const
-    {
-      return it_ > rhs.it_;
-    }
-
-    bool operator>=(const GroupIterator &rhs) const
-    {
-      return it_ >= rhs.it_;
-    }
-
-    GroupIterator operator+(difference_type n) const
-    {
-      GroupIterator tmp = *this;
-      tmp += n;
-      return tmp;
-    }
-
-    GroupIterator operator-(difference_type n) const
-    {
-      GroupIterator tmp = *this;
-      tmp -= n;
-      return tmp;
-    }
-
-    difference_type operator-(const GroupIterator &rhs) const
-    {
-      return (it_ - rhs.it_) / size_;
+    template <typename T>
+    static void Sort(std::deque<T> &deq) {
+#ifdef DEBUG_MODE
+      comparison_count = 0;
+#endif
+      GroupIterator<typename std::deque<T>::iterator> first(deq.begin(), 1);
+      GroupIterator<typename std::deque<T>::iterator> last(deq.end(), 1);
+      SortImplDeq<std::deque<T> >(first, last);
+#ifdef DEBUG_MODE
+      std::cout << "comparison count: " << comparison_count << std::endl;
+#endif
     }
 
   private:
-    Iterator it_;
-    difference_type size_;
-  };
+    template <typename Iterator>
+    class GroupIterator {
+      public:
+        typedef Iterator iterator_type;
+        typedef typename std::iterator_traits<Iterator>::difference_type difference_type;
+        typedef typename std::iterator_traits<Iterator>::value_type value_type;
+        typedef typename std::iterator_traits<Iterator>::pointer pointer;
+        typedef typename std::iterator_traits<Iterator>::reference reference;
+        typedef typename std::iterator_traits<Iterator>::iterator_category iterator_category;
 
-  template <typename Iterator>
-  static void iterSwap(GroupIterator<Iterator> lhs, GroupIterator<Iterator> rhs)
-  {
-    Iterator lhs_begin = lhs.base();
-    Iterator lhs_end = lhs.base();
-    std::advance(lhs_end, lhs.size());
-    Iterator rhs_begin = rhs.base();
+        GroupIterator(): it_(), size_(0) {}
+        GroupIterator(Iterator it, difference_type size): it_(it), size_(size) {}
+        GroupIterator(const GroupIterator &other): it_(other.it_), size_(other.size_) {}
+        GroupIterator &operator=(const GroupIterator &other) {
+          if (this == &other) { return *this; }
+          it_ = other.it_;
+          size_ = other.size_;
+          return *this;
+        }
+        ~GroupIterator() {}
 
-    std::swap_ranges(lhs_begin, lhs_end, rhs_begin);
-  }
+        iterator_type base() const { return it_; }
+        difference_type size() const { return size_; }
 
-  template <typename Iterator>
-  static void iterSwapIf(Iterator a, Iterator b)
-  {
-    if (*b < *a)
-    {
-      std::iter_swap(a, b);
+        reference operator*() const {
+          Iterator tmp = it_;
+          std::advance(tmp, size_ - 1);
+          return *tmp;
+        }
+        pointer operator->() const {
+          return  &(operator*());
+        }
+        GroupIterator &operator++() {
+          std::advance(it_, size_);
+          return *this;
+        }
+        GroupIterator &operator--() {
+          std::advance(it_, -size_);
+          return *this;
+        }
+        GroupIterator operator+(difference_type n) {
+          GroupIterator tmp = *this;
+          tmp += n;
+          return tmp;
+        }
+        GroupIterator operator-(difference_type n) {
+          GroupIterator tmp = *this;
+          tmp -= n;
+          return tmp;
+        }
+        GroupIterator &operator+=(difference_type n) {
+          it_ += size_ * n;
+          return *this;
+        }
+        GroupIterator &operator-=(difference_type n) {
+          it_ -= size_ * n;
+          return *this;
+        }
+        difference_type operator-(const GroupIterator &rhs) const {
+          return (it_ - rhs.it_) / size_;
+        }
+
+        bool operator==(const GroupIterator &rhs) const {
+          return it_ == rhs.it_;
+        }
+        bool operator!=(const GroupIterator &rhs) const {
+          return it_ != rhs.it_;
+        }
+        bool operator<(const GroupIterator &rhs) const {
+          return it_ < rhs.it_;
+        }
+        bool operator>(const GroupIterator &rhs) const {
+          return it_ > rhs.it_;
+        }
+        void iterSwap(GroupIterator other) {
+          for (difference_type i = 0; i < size_; ++i) {
+            std::iter_swap(it_ + i, other.it_ + i);
+          }
+        }
+      private:
+        Iterator it_;
+        difference_type size_;
+    };
+
+    PMergeMe();
+    PMergeMe(const PMergeMe &other);
+    PMergeMe &operator=(const PMergeMe &other);
+    ~PMergeMe();
+
+    template <typename T>
+    static T ValueInGroupIterator(T v) {
+      return v;
     }
-  }
-
-  template <typename Iterator>
-  static void iterSwapIf(GroupIterator<Iterator> a, GroupIterator<Iterator> b)
-  {
-    if (*b < *a)
-    {
-      iterSwap(a, b);
+    template <typename Iterator>
+    static typename Iterator::value_type ValueInGroupIterator(GroupIterator<Iterator> it) {
+      if (it.size() == 1) { return *it; }
+      return ValueInGroupIterator(*it);
     }
-  }
-
-  template <typename ListIter, typename GroupIter>
-  static ListIter binarySearchInsertionPoint(ListIter first, ListIter last, const GroupIter &value_iter)
-  {
-    ListIter it;
-    typename std::iterator_traits<ListIter>::difference_type count, step;
-    count = std::distance(first, last);
-
-    while (count > 0)
-    {
-      it = first;
-      step = count / 2;
-      std::advance(it, step);
-
-      // Compare dereferenced values
-      if (!(*value_iter < **it))
-      {
-        first = ++it;
-        count -= step + 1;
+    template <typename Iterator>
+    static void ListGroupIterators(GroupIterator<Iterator> first, GroupIterator<Iterator> last) {
+      for (GroupIterator<Iterator> it = first; it != last; ++it) {
+        std::cerr << ValueInGroupIterator(it) << " ";
       }
-      else
-      {
-        count = step;
+      std::cerr << std::endl;
+    }
+    template <typename Container>
+    static void ListContainer(Container c) {
+      for (size_t i = 0; i < c.size(); ++i) {
+        std::cerr << ValueInGroupIterator(c[i]) << " ";
+      }
+      std::cerr << std::endl;
+    }
+
+    template <typename Container>
+    static void BinaryInsertion(Container &chain, typename Container::value_type elem, size_t right) {
+      size_t left = 0;
+      if (chain.size() <= right) { right = chain.size() - 1; }
+
+      size_t mid = left + (right - left) / 2;
+      while (mid > 0 && left <= right) {
+        mid = left + (right - left) / 2;
+#ifdef DEBUG_MODE
+        ++comparison_count;
+        DEBUG("comparing " << ValueInGroupIterator(chain[mid]) << " and " << ValueInGroupIterator(elem));
+#endif
+        if (ValueInGroupIterator(chain[mid]) == ValueInGroupIterator(elem)) {
+          left = mid;
+          break;
+        } else if (ValueInGroupIterator(chain[mid]) < ValueInGroupIterator(elem)) {
+          left = mid + 1;
+        } else {
+          right = mid - 1;
+        }
+      }
+      chain.insert(chain.begin() + left, elem);
+    }
+
+    template <typename Container>
+    static void Insertion(Container &main_chain, Container &pend_chain) {
+      size_t total_elem = main_chain.size() + pend_chain.size();
+
+      size_t num_inserted = 1;
+      size_t prev_group_size = 0;
+      size_t curr_group_size = 2;
+      size_t upper_bound = total_elem - 1 < 4 ? total_elem - 1 : 3;
+
+      while (num_inserted + curr_group_size < pend_chain.size()) {
+        size_t index = num_inserted + curr_group_size - 1;
+        while (index >= num_inserted) {
+#ifdef DEBUG_MODE
+          DEBUG("main chain: ");
+          ListContainer(main_chain);
+          DEBUG("pend chain: "); ListContainer(pend_chain);
+          DEBUG("inserting " << ValueInGroupIterator(pend_chain[index]) << " with upper_bound: " << upper_bound -1);
+#endif
+          BinaryInsertion(main_chain, pend_chain[index], upper_bound - 1);
+          --index;
+        }
+
+        num_inserted += curr_group_size;
+        size_t next_group_size = curr_group_size + 2 * prev_group_size;
+        prev_group_size = curr_group_size;
+        curr_group_size = next_group_size;
+        upper_bound = 2 * upper_bound + 1;
+      }
+
+      if (!pend_chain.size()) { return; }
+      for (size_t index = pend_chain.size() - 1; index >= num_inserted; --index) {
+        BinaryInsertion(main_chain, pend_chain[index], upper_bound - 1);
       }
     }
-    return first;
-  }
 
-  static void sortImpl(GroupIterator<std::vector<int>::iterator> first,
-                       GroupIterator<std::vector<int>::iterator> last);
+    template <typename Container>
+    static void SortImplVec(GroupIterator<typename Container::iterator> first, GroupIterator<typename Container::iterator> last) {
+      typedef GroupIterator<typename Container::iterator> group_iter;
 
-  static void sortImpl(GroupIterator<std::deque<int>::iterator> first,
-                       GroupIterator<std::deque<int>::iterator> last);
+#ifdef DEBUG_MODE
+      DEBUG("SortImpl called with iter size " << first.size());
+      DEBUG("Sorting following elements");
+      ListGroupIterators(first, last);
+#endif
+      typename group_iter::difference_type size = std::distance(first, last);
+      if (size < 2) { return; }
 
-  PMergeMe();
-  PMergeMe(const PMergeMe &);
-  PMergeMe &operator=(const PMergeMe &);
-  ~PMergeMe();
+      bool has_stray = (size % 2 == 1);
+      group_iter end = has_stray ? last - 1 : last;
+      for (group_iter it = first; it != end; it += 2) {
+#ifdef DEBUG_MODE
+        ++comparison_count;
+#endif
+        if (*it > *(it + 1)) {
+          it.iterSwap(it + 1);
+        }
+      }
+
+      SortImplVec<Container>(group_iter(first.base(), 2 * first.size()), group_iter(end.base(), 2 * end.size()));
+
+      std::vector<group_iter> main_chain;
+      main_chain.push_back(first);
+      main_chain.push_back(first + 1);
+      std::vector<group_iter> pend_chain;
+      pend_chain.push_back(first);
+      for (group_iter it = first + 2; it != end; it += 2) {
+        main_chain.push_back(it + 1);
+        pend_chain.push_back(it);
+      }
+      if (has_stray) { pend_chain.push_back(end); }
+
+#ifdef DEBUG_MODE
+      DEBUG("Inerting elements. iter size: " << first.size());
+#endif
+      Insertion(main_chain, pend_chain);
+#ifdef DEBUG_MODE
+      ListContainer(main_chain);
+      DEBUG("Insertion completed");
+#endif
+      
+      std::vector<typename group_iter::value_type> tmp;
+      for (typename std::vector<group_iter>::iterator it = main_chain.begin(); it != main_chain.end(); ++it) {
+        for (typename group_iter::difference_type i = 0; i < it->size(); ++i) {
+          tmp.push_back(*(it->base() + i));
+        }
+      }
+      std::copy(tmp.begin(), tmp.end(), first.base());
+    }
+
+    template <typename Container>
+    static void SortImplDeq(GroupIterator<typename Container::iterator> first, GroupIterator<typename Container::iterator> last) {
+      typedef GroupIterator<typename Container::iterator> group_iter;
+
+#ifdef DEBUG_MODE
+      DEBUG("SortImpl called with iter size " << first.size());
+      DEBUG("Sorting following elements");
+      ListGroupIterators(first, last);
+#endif
+      typename group_iter::difference_type size = std::distance(first, last);
+      if (size < 2) { return; }
+
+      bool has_stray = (size % 2 == 1);
+      group_iter end = has_stray ? last - 1 : last;
+      for (group_iter it = first; it != end; it += 2) {
+#ifdef DEBUG_MODE
+        ++comparison_count;
+#endif
+        if (*it > *(it + 1)) {
+          it.iterSwap(it + 1);
+        }
+      }
+
+      SortImplDeq<Container>(group_iter(first.base(), 2 * first.size()), group_iter(end.base(), 2 * end.size()));
+
+      std::deque<group_iter> main_chain;
+      main_chain.push_back(first);
+      main_chain.push_back(first + 1);
+      std::deque<group_iter> pend_chain;
+      pend_chain.push_back(first);
+      for (group_iter it = first + 2; it != end; it += 2) {
+        main_chain.push_back(it + 1);
+        pend_chain.push_back(it);
+      }
+      if (has_stray) { pend_chain.push_back(end); }
+
+#ifdef DEBUG_MODE
+      DEBUG("Inerting elements. iter size: " << first.size());
+#endif
+      Insertion(main_chain, pend_chain);
+#ifdef DEBUG_MODE
+      ListContainer(main_chain);
+      DEBUG("Insertion completed");
+#endif
+      
+      std::deque<typename group_iter::value_type> tmp;
+      for (typename std::deque<group_iter>::iterator it = main_chain.begin(); it != main_chain.end(); ++it) {
+        for (typename group_iter::difference_type i = 0; i < it->size(); ++i) {
+          tmp.push_back(*(it->base() + i));
+        }
+      }
+      std::copy(tmp.begin(), tmp.end(), first.base());
+    }
 };
 
 #endif
--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -1,22 +1,16 @@
-#include "PMergeMe.hpp"
 #include <iostream>
 #include <sstream>
-#include <limits>
-#include <stdexcept>
 #include <iomanip>
 #include <sys/time.h>
 
-static int parsePositiveInt(const std::string &str)
-{
-  if (str.empty())
-  {
+#include "PMergeMe.hpp"
+
+int parsePositiveInt(const std::string &str) {
+  if (str.empty()) {
     throw std::runtime_error("Error: empty argument");
   }
-
-  for (size_t i = 0; i < str.length(); ++i)
-  {
-    if (!std::isdigit(str[i]))
-    {
+  for (size_t i = 0; i < str.length(); ++i) {
+    if (!std::isdigit(str[i])) {
       throw std::runtime_error("Error: invalid character");
     }
   }
@@ -24,19 +18,29 @@ static int parsePositiveInt(const std::string &str)
   std::istringstream iss(str);
   long num;
   iss >> num;
-
-  if (iss.fail() || num <= 0 || num > std::numeric_limits<int>::max())
-  {
+  if (iss.fail() || num <= 0 || num > std::numeric_limits<int>::max()) {
     throw std::runtime_error("Error: number out of range");
   }
 
   return static_cast<int>(num);
 }
 
-static void parseArguments(int argc, char **argv,
-                           std::vector<int> &vec,
-                           std::deque<int> &deq)
-{
+void displaySequence(const std::string &prefix, const std::vector<int> &container) {
+  std::cout << prefix;
+
+  size_t size = container.size();
+
+  for (size_t i = 0; i < size; ++i) {
+    std::cout << container[i];
+    if (i + 1 < size) {
+      std::cout << " ";
+    }
+  }
+
+  std::cout << std::endl;
+}
+
+void parseArguments(int argc, char **argv, std::vector<int> &vec, std::deque<int> &deq) {
   if (argc < 2)
   {
     throw std::runtime_error("Error: no arguments provided");
@@ -50,59 +54,31 @@ static void parseArguments(int argc, char **argv,
   }
 }
 
-static void displaySequence(const std::string &prefix, const std::vector<int> &container)
-{
-  std::cout << prefix;
-
-  size_t size = container.size();
-
-  for (size_t i = 0; i < size; ++i)
-  {
-    std::cout << container[i];
-    if (i + 1 < size)
-    {
-      std::cout << " ";
-    }
-  }
-
-  std::cout << std::endl;
+void displayTime(const std::string &containerName, size_t size, double microseconds) {
+  std::cout << "Time to process a range of " << size << " elements with " << containerName << " : " << std::fixed << std::setprecision(5) << microseconds << " us" << std::endl;
 }
 
-static void displayTime(const std::string &containerName, size_t size, double microseconds)
-{
-  std::cout << "Time to process a range of " << size
-            << " elements with " << containerName << " : "
-            << std::fixed << std::setprecision(5) << microseconds << " us"
-            << std::endl;
-}
-
-static double getTimeDifference(struct timeval start, struct timeval end)
-{
+double getTimeDifference(struct timeval start, struct timeval end) {
   return (end.tv_sec - start.tv_sec) * 1000000.0 + (end.tv_usec - start.tv_usec);
 }
 
-int main(int argc, char **argv)
-{
-  try
-  {
+int main(int argc, char **argv) {
+  try {
     std::vector<int> vec;
     std::deque<int> deq;
-
     parseArguments(argc, argv, vec, deq);
 
-    std::vector<int> original = vec;
-
-    displaySequence("Before: ", original);
+    displaySequence("Before: ", vec);
 
     struct timeval start_vec, end_vec;
     gettimeofday(&start_vec, NULL);
-    PMergeMe::sort(vec.begin(), vec.end());
+    PMergeMe::Sort(vec);
     gettimeofday(&end_vec, NULL);
     double time_vec = getTimeDifference(start_vec, end_vec);
 
     struct timeval start_deq, end_deq;
     gettimeofday(&start_deq, NULL);
-    PMergeMe::sort(deq.begin(), deq.end());
+    PMergeMe::Sort(deq);
     gettimeofday(&end_deq, NULL);
     double time_deq = getTimeDifference(start_deq, end_deq);
 
@@ -121,9 +97,7 @@ int main(int argc, char **argv)
     displaySequence("After:  ", vec);
     displayTime("std::vector", vec.size(), time_vec);
     displayTime("std::deque", deq.size(), time_deq);
-  }
-  catch (const std::exception &e)
-  {
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
     return 1;
   }
